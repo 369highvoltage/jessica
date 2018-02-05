93224f39b27894d949be38c225091cc49707ed56
Stephon Lawrence <stephonlawrence@gmail.com>
2018-02-05 14:46:27 -0500

    first robot setup

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Jessica - 2018 Robot Code
+## Run Commands
+### Deploy
+```commandline
+python robot.py --deploy --skip-tests
+```
---
+++ robot.py
@@ -0,0 +1,10 @@
+from wpilib import IterativeRobot, run
+
+
+class Jessica(IterativeRobot):
+    def robotInit(self):
+        pass
+
+
+if __name__ is '__main__':
+    run(Jessica)
