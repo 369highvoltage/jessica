ae9c5c1189814d8f0fbfe291bfacee242b56e903
Treayn <Treayn@gmail.com>
2018-02-10 18:35:48 -0500

    added states

--- robot.py
+++ robot.py
@@ -2,9 +2,27 @@ from wpilib import IterativeRobot, run
 
 
 class Jessica(IterativeRobot):
-    def robotInit(self):
+
+    # Create motors and stuff here
+    def createObjects(self):
         pass
 
+    def robotInit(self):
+        pass
+    
+    # Init: Called when mode starts; optional 
+    # Periodic: Called on each iteration of the control loop
+    def autonomousInit(self):
+        pass
+    
+    def autonomousPeriodic(self):
+        pass
+
+    def teleopInit(self):
+        pass
+    
+    def teleopPeriodic(self):
+        pass
 
 if __name__ is '__main__':
     run(Jessica)
